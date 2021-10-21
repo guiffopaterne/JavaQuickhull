dd5295018b2215d4e7a84f6c6967af4a46d6d8ae
guiffopaterne <guiffopaterne@gmail.com>
2021-10-21 11:43:40 +0100

    add jarvis algorithm and optimize quickhull algorithme

--- src/algorithm/QuickHull.java
+++ src/algorithm/QuickHull.java
@@ -8,19 +8,30 @@ import java.awt.Point;
 
 // creation de la classe responsable de l'agorithme du quickhull
 public class QuickHull{
+	 public int orientation(Point p, Point q, Point r)
+	    {
+	        int val = (q.y - p.y) * (r.x - q.x) -
+	                  (q.x - p.x) * (r.y - q.y);
+	        if (val == 0) return 0;
+	        return (val > 0)? 1: 2; 
+	    }
+
 //	les donnes d'entree est une listes de pounts
 	private ArrayList<Point> point = new ArrayList<Point>();
 //	donnees de sortie de l'algorithme les points de l'enveloppe convexe
 	public ArrayList<Point> hull = new ArrayList<Point>();
+	public ArrayList<Point> hullJavis = new ArrayList<Point>();
 	// la taille de la liste
 	private int n = 0;
+	private int min;
 	
 // SearchSide la fonction qui determine de quel cote de 
 //	la droite se trouve le point
 //	nous permet de decide si ce point est du bon cote ou si doit encore prs en consideration
 	
 	public Integer searchSide(Point a,Point b,Point p){
-		Integer value = (p.y - a.y)*(b.x - a.x)-(b.y-a.y)*(p.x-b.x);
+		Integer value = (p.y - a.y)*(b.x - a.x)-(b.y-a.y)*(p.x-a.x);
+		
 		if(value>0) {
 			return 1;
 		}
@@ -31,13 +42,31 @@ public class QuickHull{
 	}
 //	mesure la distance entre un point p et 
 //	le segment de droite cree pas les points a et b
-	
+	private Double distanceToSegment(Point ps, Point pe, Point p) {
+		if (ps.x==pe.x && ps.y==pe.y) return this.distance(ps,p);
+		long sx=pe.x-ps.x,sy=pe.y-ps.y,ux=p.x-ps.x,uy=p.y-ps.y;
+		long dp=sx*ux+sy*uy;
+		if (dp<0) return this.distance(ps,p);
+		long sn2 = sx*sx+sy*sy;
+		if (dp>sn2) return this.distance(pe,p);
+	 
+		double ah2 = dp*dp / sn2;
+		long un2=ux*ux+uy*uy;
+		return Math.sqrt(un2-ah2);
+	}
+	 
+	/**
+	 * return the distance between two points
+	 *
+	 * @param p1,p2 the two points
+	 * @return dist the distance
+	 **/
+	private Double distance(Point p1, Point p2) {
+		long d2 = (p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y);
+		return Math.sqrt(d2);
+	}
 	public int distance(Point a,Point b,Point p){
-		int value = (p.y - a.y)*(b.x - a.x)-(b.y-a.y)*(p.x-b.x);
-		if(value>=0) {
-			return value;
-		}
-		return value*-1;
+		return Math.abs(p.y - a.y)*(b.x - a.x)-(b.y-a.y)*(p.x-b.x);
 	}
 //	connaitre si le point est interne au triangle
 	public int insideTriangle(Point a,Point b,Point c,Point p) {
@@ -60,13 +89,12 @@ public class QuickHull{
 //	Algorithme du quickhull
 	public void quickHull(Point A,Point B,int side){
 		this.n = this.point.size();
-//		 @find:int pour determiner si on a trouve un point
-//		max_dist : recuperer le maximun de distance
-	    int find = -1, max_dist =0;
+	    int find = -1;
+	    		double max_dist =-1;
 	    for(int i=0;i< this.n; i++) {
-	    	int temp= this.distance(A, B, this.point.get(i));
+	    	Double temp= this.distanceToSegment(A, B, this.point.get(i));
 	    	int side_local = this.searchSide(A, B, this.point.get(i));
-	    	if(side == side_local && temp>max_dist) {
+	    	if((side == side_local)&& temp>max_dist) {
 	    		find = i;
 	    		max_dist = temp;
 	    	}
@@ -76,20 +104,29 @@ public class QuickHull{
 	    	hull.add(A);
 	    	return;
 	    }
-	    else {
-	    	hull.add(this.point.get(find));
-//	    	System.out.format(Locale.FRANCE, "Les points qui sont a linterrieur du triangle forme par {A:%s,\n B: %s,\n P :%s} sont :\n",A.toString(),B.toString(),this.point.get(find).toString());
-//	    	for(int i=0;i< this.n; i++) {
-//	    		if(this.insideTriangle(A, B, this.point.get(find), this.point.get(i))>0) {
-//	    			System.out.format(Locale.FRANCE, "{%s}\n",this.point.get(i).toString());
-//	    		}
-//	    	}
-    }
-	    
 //	    la recursivite 
 	    
 	    this.quickHull(this.point.get(find),A,-this.searchSide(this.point.get(find), A, B));
 	    this.quickHull(this.point.get(find),B,-this.searchSide(this.point.get(find), B, A));
+	}
+	public void Jarvis() {
+		int p = this.min, q, ii = 0;
+		do
+		{
+			this.hullJavis.add(this.point.get(p));
+			q = (p + 1) % n;
+			for (int i=0; i<this.n; i++) {
+				int dist = this.orientation(this.point.get(p), this.point.get(i), this.point.get(q));
+				if(dist==2) q=i;
+			}
+			p=q;
+		}while(p != this.min);
+		
+	}
+	public QuickHull(ArrayList <Point> points, int min){
+		this.point = points;
+		this.n = points.size();
+		this.min = min;
 	}
 	public QuickHull(ArrayList <Point> points){
 		this.point = points;
--- src/algorithm/main.java
+++ src/algorithm/main.java
@@ -9,7 +9,6 @@ import java.io.*;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.Date;
-//import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -19,9 +18,10 @@ public class main {
 	    return random.nextInt(max - min) + min;
 	}
 	public static boolean isWindows() {
-		return (System.getProperty("os.name")=="Windows")?true:false;
+		String os = System.getProperty("os.name");
+		return os.startsWith("W");
 	}
-	public static String pathFileCsv(String Filename) {
+	public static String pathFileCsv(String Filename,String directory) {
 		Path path = FileSystems.getDefault().getPath("").toAbsolutePath();
 //		pour teste
 		System.out.println("le dossier est:"+path);
@@ -29,19 +29,18 @@ public class main {
 		System.out.println("les systems est :"+ os);
 //		code de fin test
 		String separator=isWindows()?"\\":"/";
-		return (path.toString()+separator+"data"+separator+Filename);
+		return (path.toString()+separator+"data"+separator+directory+separator+Filename);
 	}
 	public static void generateCsv(String Filename, int col,int row) {
-		String absoluPathFile = pathFileCsv(Filename);
+		String absoluPathFile = pathFileCsv(Filename,"generate");
 		System.out.println("le chemin absolu  est :"+ absoluPathFile);
 		File ouputFile = new File(absoluPathFile);
 		try {
 			PrintWriter writer = new PrintWriter(ouputFile);
 			for(int i=0;i<row;i++) {
 				for(int j=0;j<col;j++) {
-					writer.printf("%d,%d",getRandomNumberUsingNextInt(col,2*row),getRandomNumberUsingNextInt(col,2*row));
+					writer.printf("%d,%d\n",getRandomNumberUsingNextInt(col,2*row),getRandomNumberUsingNextInt(col,2*row));
 				}
-				writer.printf("\n");
 			}
 			writer.close();
 		}catch(FileNotFoundException e1) {
@@ -52,7 +51,7 @@ public class main {
 	public static ArrayList <Point> scannerCsv(String filename){
 		ArrayList <Point> hull = new ArrayList <Point>(); 
 		BufferedReader br = null;
-		String absoluPathFile = pathFileCsv(filename);
+		String absoluPathFile = pathFileCsv(filename,"generate");
 		try {
 			br = new BufferedReader(new FileReader(absoluPathFile));
 		} catch (FileNotFoundException e1) {
@@ -84,38 +83,86 @@ public class main {
 			System.out.format(Locale.FRANCE,"{%d:%d}\n",newList.get(i).x,newList.get(i).y);
 		}
 	}
-	
-	public static void main(String[] args){
+	public static long[] algoritm(int interation) {
 		ArrayList <Point> points = new ArrayList<Point>();
 		String Filename = "output.csv";
-		generateCsv(Filename,2,250);
+//		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+		Date d1 = null, d2=null,d3 = null;
+		generateCsv(Filename,2,interation);
 		points = scannerCsv(Filename);
-		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-		QuickHull conver = new QuickHull(points);
 		int n = points.size();
+		
+		int min_x = 0, max_x = 0;
+	    for (int i=1; i<n; i++){
+	        if (points.get(i).x <  points.get(min_x).x)
+	            min_x = i;
+	        if (points.get(i).x > points.get(max_x).x)
+	            max_x = i;
+	        if(points.get(i).x ==  points.get(min_x).x){
+	        	if(points.get(i).y <  points.get(min_x).y) min_x=i;
+	        }
+	        if (points.get(i).x == points.get(max_x).x) {
+	        	if (points.get(i).y > points.get(max_x).y)
+		            max_x = i;
+	        }
+	    }
+	    System.out.println("debut KB: " + (double) (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024);
+		QuickHull conver = new QuickHull(points,min_x);
+		
 		System.out.format("le nombre des points dans l'espace sont %d\n",n);
-		Date d1 = Calendar.getInstance().getTime();
+		
 		if(n<=3) {
 			affiche_resultat(points);
 		}else {
-		    int min_x = 0, max_x = 0;
-		    for (int i=1; i<n; i++){
-		        if (points.get(i).x <  points.get(min_x).x)
-		            min_x = i;
-		        if (points.get(i).x > points.get(max_x).x)
-		            max_x = i;
-		    }
 		    System.out.format("le minimun est %s\n",points.get(min_x).toString());
 		    System.out.format("le maximun est %s\n",points.get(max_x).toString());
 		    conver.hull.add(points.get(min_x));
 		    conver.hull.add(points.get(max_x));
-		    conver.quickHull(points.get(min_x),points.get(max_x),1);
+		    d1 = Calendar.getInstance().getTime();
 		    conver.quickHull(points.get(min_x),points.get(max_x),-1);
+		    conver.quickHull(points.get(min_x),points.get(max_x),1);
+		    d2 = Calendar.getInstance().getTime();
+		    System.out.println("apres hull KB: " + (double) (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024);
+		    conver.Jarvis();
+		    d3 = Calendar.getInstance().getTime();
+		    System.out.println("jarvis KB: " + (double) (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024);
 		}
-		Date d2 = Calendar.getInstance().getTime();
+		
 		long duration = d2.getTime() - d1.getTime();
 		long duree = TimeUnit.MILLISECONDS.toMillis(duration);
 		affiche_resultat(conver.hull);
 		System.out.println("Temps d'excecution est de "+duree+" MilliSeconde");
+		System.out.println("==============================================================\n Jarvis interactif algorithm");
+		long duration_jarvis = d3.getTime() - d2.getTime();
+		long duree_jarvis = TimeUnit.MILLISECONDS.toMillis(duration_jarvis);
+		affiche_resultat(conver.hullJavis);
+		System.out.println("Temps d'excecution est de "+duree_jarvis+" MilliSeconde");
+		return new long[] {n,duree,duree_jarvis};
+	}
+	
+	public static void main(String[] args){
+//		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+		int interation =50;
+		String absoluPathFile = pathFileCsv("statisc.csv","out");
+		System.out.println("le chemin absolu  est :"+ absoluPathFile);
+		File ouputFile = new File(absoluPathFile);
+		PrintWriter writer;
+		long[] a = new long [3];
+		try {
+			writer = new PrintWriter(ouputFile);
+			writer.printf("nombre_element,quickhull_time,jarvis_time\n");
+			for(int count=0;count<10;count++) {
+				a=algoritm(interation);
+				writer.printf("%d,%d,%d\n",a[0],a[1],a[2]);
+				interation+=50;
+			}
+			writer.close();
+		} catch (FileNotFoundException e) {
+			for(int count=0;count<10;count++) {
+				a=algoritm(interation);
+				interation+=50;
+			}
+			e.printStackTrace();
 		}
 	}
+}
